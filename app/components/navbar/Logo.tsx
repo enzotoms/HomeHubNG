ed7b0a86021b8e4f4c174c90b8cb52961c2b5803
enzotoms <135044593+enzotoms@users.noreply.github.com>
2024-07-23 14:26:45 +0100

    Update Logo.tsx

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -13,7 +13,7 @@ const Logo = () => {
              className="hidden md:block cursor-pointer"
               width="100"
                height="100" 
-               src="/images/logo.png"
+               src="/images/pngegg.png"
                />
            </div>
     )
